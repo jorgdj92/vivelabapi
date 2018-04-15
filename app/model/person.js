e4d96330843b5d4af8a7ff89c665b04316635bd2
jorgdj92 <jorgeivanreyesvalencia92@gmail.com>
2018-04-15 11:31:19 -0500

    Add files via upload

--- app/model/person.js
+++ app/model/person.js
@@ -1,1 +1,10 @@
+var mongoose = require('mongoose')
+require('mongoose-long')(mongoose);
+var SchemaTypes = mongoose.Schema.Types;
 
+module.exports = mongoose.model('Person', {
+	personId: SchemaTypes.Long,
+	firstName: String,
+	lastName: String,
+	email: String
+});
